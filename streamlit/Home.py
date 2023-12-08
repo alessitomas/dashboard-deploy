d97f5c2a793e133b708e4f41da031cf4013cd1c9
Alessi181 <tomasalessi@gmail.com>
2023-12-08 11:10:08 -0300

    changes to the deploy repository

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -2,6 +2,7 @@ import streamlit as st
 import sys
 import os
 
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '', 'utils')))
 
 from helper_functions import create_user, logged_out_option,  verify_credentials, add_log
